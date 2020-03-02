afa5bdc4d5bd48e65cc469473d1995265d3c2932
Daniel Kraus <bovender@bovender.de>
2020-03-02 07:37:53 +0100

    Fix creating links and install Node packages.

--- bootstrap-container.sh
+++ bootstrap-container.sh
@@ -18,24 +18,30 @@ fi
 echo "Bootstrapping container... $(date --rfc-3339=seconds)"
 set -x -e
 
-git clone --depth 1 -b $GIT_BRANCH https://${GIT_USER%% }${GIT_USER:+:}${GIT_PASS%% }${GIT_USER:+@}${GIT_REPO#https://} $APP_DIR
+git clone --depth 1 -b $GIT_BRANCH https://${GIT_USER%% }${GIT_USER:+:}${GIT_PASS%% }${GIT_USER:+@}${GIT_REPO#https://} "$APP_DIR" ||
+	(echo "Directory `$APP_DIR` exists already, attempting to pull..."; git -C "$APP_DIR" pull)
 chown -R app:app ${APP_DIR}
 
 chown -R app:app /shared
 
 # NB: When invoked with the `-p` flag, mkdir will not
 # raise an error if the directory exists already.
-mkdir -p /shared/{bundle,log,uploads}
+# We keep gems and node modules out of the container
+# for faster rebuilding.
+mkdir -p /shared/{bundle,log,node_modules,uploads}
 
 cd $APP_DIR
-ln -sf /shared/bundle vendor/bundle &&\
-ln -sf /shared/log log
-ln -sf /shared/uploads uploads
+rm -rf vendor/bundle && ln -s /shared/bundle vendor/bundle
+for d in log node_modules uploads; do
+  rm -rf $d
+  ln -s /shared/$d $d
+done
 setuser app bundle config set deployment true
 setuser app bundle config set with production
 setuser app bundle config set without test:development
 setuser app bundle install
 setuser app bundle exec rails db:migrate
+setuser app yarn install --check-files
 setuser app bundle exec rails assets:precompile
 
 set +x
