af81e106fd385f60af07dd1993071532af64badf
Daniel Kraus <bovender@bovender.de>
2021-05-03 22:37:15 +0200

    Source container_environment.sh upon login.

--- bootstrap-container.sh
+++ bootstrap-container.sh
@@ -84,6 +84,9 @@ fi
 
 export RAILS_ENV=$PASSENGER_APP_ENV
 echo $RAILS_ENV > /etc/container_environment/RAILS_ENV
+sed -i '/source \/etc\/container_environment\.sh/d' /root/.bashrc
+echo 'source /etc/container_environment.sh' >> /root/.bashrc
+
 setuser dora bundle config --local path vendor/bundle
 setuser dora bundle config --local deployment $BUNDLE_DEPLOY
 setuser dora bundle config --delete without
